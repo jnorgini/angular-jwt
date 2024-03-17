ad048aecde8b4cb13df4613699307bd630d1a640
jnorgini <jnorgini@gmail.com>
2024-03-17 14:59:06 -0300

    feat: login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { Login } from '../models/Login';
+import { Register } from '../models/Register';
+import { JwtAuth } from '../models/JwtAuth';
+import { AuthenticationService } from '../services/authentication.service';
+import { User } from '../models/User';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  loginDto = new Login();
+  registerDto = new Register();
+  jwtDto = new JwtAuth();
+  currentUser: User | undefined;
+  currentUserVisible: boolean = false;
+
+  constructor(private authService: AuthenticationService, private router: Router) { }
+
+  register(registerDto: Register) {
+    this.authService.register(registerDto).subscribe((userData: any) => {
+      console.log(userData);
+    });
+  }
+
+  login(loginDto: Login) {
+    this.authService.login(loginDto).subscribe((jwtDto) => {
+      localStorage.setItem('jwtToken', jwtDto.token);
+      console.log('Token: ', jwtDto.token, '\nRefreshToken: ', jwtDto.refreshToken, '\nUsername: ', jwtDto.username);
+      this.userDetails();
+      this.router.navigate(['/main']);
+    });
+  }
+
+  userDetails(): void {
+    this.authService.getCurrentUser()
+      .subscribe(user => {
+        this.currentUser = user;
+      });
+  }
+
+}
