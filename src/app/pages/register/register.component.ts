bb0b6af08d0d658139d81748ff97526417d2d6f8
jnorgini <jnorgini@gmail.com>
2024-03-19 01:42:37 -0300

    fix: register validations

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -3,7 +3,6 @@ import { Register } from '../../models/Register';
 import { JwtAuth } from '../../models/JwtAuth';
 import { AuthenticationService } from '../../services/authentication.service';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -13,18 +12,16 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class RegisterComponent {
   registerDto = new Register();
   jwtDto = new JwtAuth();
-  touchedFirstname = false;
-  touchedLastname = false;
-  touchedEmail = false;
-  touchedUsername = false;
-  touchedPassowrd = false;
-  touchedConfirmPassword = false;
-  touchedRole = false;
-  touchedStatus = false;
+  validation = false;
 
   constructor(private authService: AuthenticationService, private router: Router) { }
 
   register(registerDto: Register) {
+    this.validation = true;
+    if (!registerDto.firstName || !registerDto.lastName || !registerDto.username || !registerDto.email || !registerDto.password || !registerDto.confirmPassword || !registerDto.role || !registerDto.status) {
+      alert('Por favor, preencha todos os campos.');
+      return;
+    }
     this.authService.register(registerDto).subscribe((userData: any) => {
       console.log(userData);
     });
