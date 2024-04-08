3e398d3dce0ba1b316a252ae26e2ea617221d75d
jnorgini <jnorgini@gmail.com>
2024-04-08 20:53:21 -0300

    feat: user update

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Login } from '../models/Login';
-import { Register } from '../models/Register';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { JwtAuth } from '../models/JwtAuth';
@@ -17,18 +16,26 @@ const HTTP_OPTIONS = {
 })
 export class AuthenticationService {
   API_URL = environment.API_URL;
-  REGISTER_URL = `${this.API_URL}/users`;
+  USER_URL = `${this.API_URL}/users`;
   LOGIN_URL = `${this.API_URL}/login`;
-  USER_URL = `${this.API_URL}/users/me`;
 
   constructor(private http: HttpClient) { }
 
-  register(user: Register): Observable<JwtAuth> {
-    return this.http.post<JwtAuth>(`${this.REGISTER_URL}`, user, HTTP_OPTIONS);
+  register(user: User): Observable<JwtAuth> {
+    return this.http.post<JwtAuth>(`${this.USER_URL}`, user, HTTP_OPTIONS);
+  }
+
+  update(updatedUser: User): Observable<User> {
+    const url = `${this.USER_URL}/${updatedUser.id}`;
+    return this.http.put<User>(url, updatedUser);
   }
 
   login(user: Login): Observable<JwtAuth> {
     return this.http.post<JwtAuth>(`${this.LOGIN_URL}`, user, HTTP_OPTIONS);
+  }
+
+  getUserById(id: number): Observable<User> {
+    return this.http.get<User>(`${this.USER_URL}/${id}`);
   }
 
   logout() {
@@ -44,7 +51,7 @@ export class AuthenticationService {
     if (token) {
       HTTP_OPTIONS.headers = HTTP_OPTIONS.headers.set('Authorization', `Bearer ${token}`);
     }
-    return this.http.get<User>(`${this.USER_URL}`, HTTP_OPTIONS);
+    return this.http.get<User>(`${this.USER_URL}/me`, HTTP_OPTIONS);
   }
 
 }
