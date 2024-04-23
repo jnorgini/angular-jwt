e35faad0ee5f0f398285ab79bf592f0b4ec92045
jnorgini <jnorgini@gmail.com>
2024-04-23 04:18:59 -0300

    refactor: sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -9,8 +9,6 @@ import { User } from '../../models/User';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
-  isSubMenuHidden = true;
-  isArrowUp = false;
   isSidebarOpen!: boolean;
   users: User[] = [];
   selectedUser: User | null = null;
@@ -18,8 +16,6 @@ export class SidebarComponent implements OnInit {
   constructor(private authService: AuthenticationService, private router: Router) { }
 
   ngOnInit() {
-    const storedSidebarState = localStorage.getItem('sidebarState');
-    this.isSidebarOpen = storedSidebarState ? JSON.parse(storedSidebarState) : false;
     this.getUsers();
   }
 
@@ -36,17 +32,10 @@ export class SidebarComponent implements OnInit {
 
   open() {
     this.isSidebarOpen = true;
-    localStorage.setItem('sidebarState', JSON.stringify(this.isSidebarOpen));
   }
 
   close() {
     this.isSidebarOpen = false;
-    localStorage.setItem('sidebarState', JSON.stringify(this.isSidebarOpen));
-  }
-
-  dropdown() {
-    this.isSubMenuHidden = !this.isSubMenuHidden;
-    this.isArrowUp = !this.isArrowUp;
   }
 
   logout() {
